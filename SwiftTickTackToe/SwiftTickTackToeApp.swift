a3b719d50cd78d5079f157777dff230be371542c
konstantinasz <konstantinas.zaveckas@gmail.com>
2023-08-08 13:41:17 +0300

    Initial Commit

---
+++ SwiftTickTackToe/SwiftTickTackToeApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftTickTackToeApp.swift
+//  SwiftTickTackToe
+//
+//  Created by Konstantinas Zaveckas on 2023-08-08.
+//
+
+import SwiftUI
+
+@main
+struct SwiftTickTackToeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
